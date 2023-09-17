8f8dac15c6e99f0fdbcc0ff8dc8c41e0a7152dff
vivihm <viviana.marca@sptech.school>
2023-09-17 16:05:49 -0300

    aaaaaaaa

--- PannyPio/script.js
+++ PannyPio/script.js
@@ -13,3 +13,61 @@ document.querySelectorAll('.navbar-link').forEach((n) =>
     navMenu.classList.remove('active');
   })
 );
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".slider");
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    const baseTransform = -index * 20; // Valor base do transform (20% por slide)
+    slider.style.transform = `translateX(${baseTransform}%)`;
+  }
+
+  function updateMaxIndex() {
+    const tamanhoVisivel = window.innerWidth;
+    console.log("olhaa" + tamanhoVisivel)
+
+    const tamanhoCard = slider.querySelector(".card").offsetWidth;
+
+    if (tamanhoVisivel < 1050) {
+      maxIndex = Math.floor(tamanhoVisivel / tamanhoCard) + 2;
+    } else if(tamanhoVisivel < 1290){
+      maxIndex = Math.floor(tamanhoVisivel / tamanhoCard) + 1;
+    } else{
+      maxIndex = Math.floor(tamanhoVisivel / tamanhoCard);
+    }
+
+    return maxIndex;
+  }
+
+  function proximoSlide() {
+    const maxIndex = updateMaxIndex();
+
+    if (currentIndex < maxIndex) {
+      currentIndex++;
+      showSlide(currentIndex);
+    } else {
+      console.log("acabou");
+    }
+  }
+
+  function anteriorSlide() {
+    if (currentIndex > 0) {
+      currentIndex--;
+      showSlide(currentIndex);
+    }
+  }
+
+  // Adicione event listeners para os botões de próximo e anterior
+  document.querySelector("#botaoProximo").addEventListener("click", proximoSlide);
+  document.querySelector("#botaoAnterior").addEventListener("click", anteriorSlide);
+
+  // Atualiza o maxIndex quando a janela for redimensionada
+  window.addEventListener("resize", function () {
+    currentIndex = 0;
+    showSlide(currentIndex);
+  });
+
+  // Exibe o primeiro slide inicialmente
+  showSlide(0);
+});
